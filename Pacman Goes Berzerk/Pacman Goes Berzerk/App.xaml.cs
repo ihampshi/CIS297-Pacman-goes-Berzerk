aac6bbad800fa534dd441d11aec91bb853614231
ihampshi <46584868+ihampshi@users.noreply.github.com>
2019-04-25 13:29:13 -0400

    Main menu page loaded first

--- Pacman Goes Berzerk/Pacman Goes Berzerk/App.xaml.cs
+++ Pacman Goes Berzerk/Pacman Goes Berzerk/App.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Pacman_Goes_Berzerk.Pages;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -66,7 +67,7 @@ namespace Pacman_Goes_Berzerk
                     // When the navigation stack isn't restored navigate to the first page,
                     // configuring the new page by passing required information as a navigation
                     // parameter
-                    rootFrame.Navigate(typeof(MainPage), e.Arguments);
+                    rootFrame.Navigate(typeof(MainMenuPage), e.Arguments);
                 }
                 // Ensure the current window is active
                 Window.Current.Activate();
